03df1f4d46648640e9d527b85c0328c24f379b4c
Muhammad Fikri <fikswznd@gmail.com>
2013-11-07 11:38:53 +0700

    Add ajax request to get content from woobox scrapper

--- sites/photo-contest/themes/assets/js/js.js
+++ sites/photo-contest/themes/assets/js/js.js
@@ -9,4 +9,20 @@ $(function() {
   });
   
   $('#nav-primary a').smoothScroll({offset: -30});
+  
+  $.getJSON('scrapper/woobox.php', function(returns) {
+    var url = 'http://woobox.com/4mn57p/vote';
+    $('#gallery .loading').hide();
+    var ul = $('<ul />');
+    $.each(returns, function() {
+      var li = $('<li />').appendTo(ul);
+      var a = $('<a />', { href: url }).appendTo(li);
+      var img = $('<div />', { class: 'img-container', style: 'background: url('+this.url+') no-repeat scroll center center / cover  transparent' }).appendTo(a);
+      var p = $('<p />', { text: this.caption }).appendTo(a);
+      
+      ul.appendTo('#gallery');
+    })
+    
+    $('<a />', { href: url, html: 'View more &rarr;', class: 'more'}).appendTo('#gallery');
+  })
 });
